c746b7d6c67332df9340e8a480ae2878c7d77b2e
crestrepoq <68136553+crestrepoq@users.noreply.github.com>
2021-02-28 00:26:50 -0500

    example of strings and beginning with last task of playing data science rol

--- date_and_time_string_examples.sql
+++ date_and_time_string_examples.sql
@@ -39,3 +39,38 @@ SELECT Birthdate
 ,STRFTIME('%d',Birthdate) AS Day
 ,DATE(('now') - Birthdate) AS Age
 FROM employees
+
+/*
+A little more complex examples
+*/
+
+SELECT e.FirstName
+    ,e.LastName
+    ,e.HireDate
+    ,STRFTIME('%Y',e.HireDate) AS HireYear
+    ,STRFTIME('%Y','now') AS ActualYear
+    ,subq.WorkingYears
+FROM Employees e
+LEFT JOIN (SELECT b.EmployeeId
+                ,(STRFTIME('%Y','now') - STRFTIME('%Y',b.HireDate)) AS WorkingYears
+            FROM Employees b
+            WHERE (STRFTIME('%Y','now') - STRFTIME('%Y',b.HireDate)) >= 15) AS subq
+    ON e.EmployeeId = subq.EmployeeId
+ORDER BY e.LastName ASC
+
+/* Result 
+----
++-----------+----------+---------------------+----------+------------+--------------+
+| FirstName | LastName | HireDate            | HireYear | ActualYear | WorkingYears |
++-----------+----------+---------------------+----------+------------+--------------+
+| Andrew    | Adams    | 2002-08-14 00:00:00 | 2002     | 2021       |           19 |
+| Laura     | Callahan | 2004-03-04 00:00:00 | 2004     | 2021       |           17 |
+| Nancy     | Edwards  | 2002-05-01 00:00:00 | 2002     | 2021       |           19 |
+| Steve     | Johnson  | 2003-10-17 00:00:00 | 2003     | 2021       |           18 |
+| Robert    | King     | 2004-01-02 00:00:00 | 2004     | 2021       |           17 |
+| Michael   | Mitchell | 2003-10-17 00:00:00 | 2003     | 2021       |           18 |
+| Margaret  | Park     | 2003-05-03 00:00:00 | 2003     | 2021       |           18 |
+| Jane      | Peacock  | 2002-04-01 00:00:00 | 2002     | 2021       |           19 |
++-----------+----------+---------------------+----------+------------+--------------+
+----
+*/
--- working_with_text_strings.sql
+++ working_with_text_strings.sql
@@ -52,5 +52,45 @@ SELECT LOWER(column_name) FROM table_name;
 SELECT UCASE(column_name) FROM table_name;
 
 
+/*
+Another more complex examples
+*/
 
+SELECT c.CustomerId
+    ,c.FirstName || ' ' || c.LastName AS FullName
+    ,c.Address
+    ,UPPER(c.City || ' ' || c.Country) AS 'City, Country'
+FROM Customers c
+WHERE c.CustomerId = 16
 
+/* Result
++------------+--------------+---------------------------+-------------------+
+| CustomerId | FullName     | Address                   | City, Country     |
++------------+--------------+---------------------------+-------------------+
+|         16 | Frank Harris | 1600 Amphitheatre Parkway | MOUNTAIN VIEW USA |
++------------+--------------+---------------------------+-------------------+
+*/
+
+-----
+SELECT e.FirstName
+    ,e.LastName
+    ,LOWER(SUBSTR(e.FirstName,1,4)) AS subuser_id1
+    ,LOWER(SUBSTR(e.LastName,1,2)) AS subuser_id2
+    ,LOWER(SUBSTR(e.FirstName,1,4) || SUBSTR(e.LastName,1,2)) AS user_id
+FROM Employees e
+
+/* Result
+
+FirstName | LastName | subuser_id1 | subuser_id2 | user_id |
++-----------+----------+-------------+-------------+---------+
+| Andrew    | Adams    | andr        | ad          | andrad  |
+| Nancy     | Edwards  | nanc        | ed          | nanced  |
+| Jane      | Peacock  | jane        | pe          | janepe  |
+| Margaret  | Park     | marg        | pa          | margpa  |
+| Steve     | Johnson  | stev        | jo          | stevjo  |
+| Michael   | Mitchell | mich        | mi          | michmi  |
+| Robert    | King     | robe        | ki          | robeki  |
+| Laura     | Callahan | laur        | ca          | laurca 
+
+*/
+-----
